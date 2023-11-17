49709cf5c03e46855ab14a490ed1cfa60fcac6f8
yar-ermolaev <yar.ermolaev@gmail.com>
2023-11-17 09:04:05 +0300

    Create models Products and Category

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    name = models.CharField(max_length=100, unique=True,
+                            verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0,
+                                verbose_name='Цена')
+    in_stock = models.PositiveIntegerField(default=0,
+                                           verbose_name='Остаток на складе')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True,
+                            verbose_name='Название')
+
+    def __str__(self):
+        return self.name
