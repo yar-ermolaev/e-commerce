6bea3ff7630098a3864a6be9965b74d00c6c17a1
yar-ermolaev <yar.ermolaev@gmail.com>
2023-11-26 19:02:11 +0300

    Change Product model - description could be blank or null

--- products/models.py
+++ products/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0,
                                 verbose_name='Цена')
     properties = models.JSONField(blank=True, verbose_name='Характеристики')
-    description = models.TextField(verbose_name='Описание')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
     image_paths = models.JSONField(blank=True, verbose_name='Список изображений')
     category = models.ForeignKey('Category', on_delete=models.PROTECT)
 
