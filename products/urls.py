93aecd81f2aa29e89b481242875f5fa97f45daff
yar-ermolaev <yar.ermolaev@gmail.com>
2023-11-28 13:20:01 +0300

    Add view, template and url for category details

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.CategoryList.as_view()),
-    path('<slug:slug>/', views.ProductView.as_view()),
+    path('<slug:slug>/', views.CategoryDetails.as_view(), name='category_detail'),
+    path('<slug:cat_slug>/<slug:slug>/', views.ProductView.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -17,3 +17,17 @@ class CategoryList(ListView):
     template_name = 'products/show_categories.html'
     context_object_name = 'categories'
 
+
+class CategoryDetails(DetailView):
+    model = Category
+    template_name = 'products/category_details.html'
+    context_object_name = 'category'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = self.object.products.all()[:10]
+        return context
+
+
