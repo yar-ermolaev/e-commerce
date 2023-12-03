c94ddd3e775eb34c2253790152b56d22abd333d9
yar-ermolaev <yar.ermolaev@gmail.com>
2023-12-03 17:45:22 +0300

    Refactor templates - add base.html

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CategoryList.as_view()),
+    path('', views.CategoryList.as_view(), name='index'),
     path('<slug:slug>/', views.CategoryDetails.as_view(), name='category_detail'),
     path('<slug:cat_slug>/<slug:slug>/', views.ProductView.as_view(), name='product_detail'),
 ]
