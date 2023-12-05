e3fc5892b7e295aaa61274e0dd53610579543a39
yar-ermolaev <yar.ermolaev@gmail.com>
2023-12-05 10:05:44 +0300

    Add login view, template, url. Change base.html

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class LoginUser(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'users/login.html'
+    extra_context = {'title': 'Авторизация'}
+
+    def get_success_url(self):
+        return reverse_lazy('index')
