11fbf57f7bf4fc16f5c34e5b28ed6903008ceaf5
yar-ermolaev <yar.ermolaev@gmail.com>
2023-12-05 10:04:34 +0300

    Refactor products.urls - to avoid Ñoverlap of patterns with /users/login

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.CategoryList.as_view(), name='index'),
-    path('<slug:slug>/', views.CategoryProducts.as_view(), name='category_detail'),
-    path('<slug:cat_slug>/<slug:slug>/', views.ProductView.as_view(), name='product_detail'),
+    path('categories/<slug:slug>/', views.CategoryProducts.as_view(), name='category_detail'),
+    path('categories/<slug:cat_slug>/<slug:slug>/', views.ProductView.as_view(), name='product_detail'),
 ]
