f0c543577f0dacf43fe6bc8f9f78930f580e22fd
yar-ermolaev <yar.ermolaev@gmail.com>
2023-12-06 16:40:10 +0300

    Create models: Cart, CartItem

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='cart')
+    products = models.ManyToManyField('products.Product', through='CartItem')
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
