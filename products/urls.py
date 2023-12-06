81071f2d7c7c563675d0faa0670bfdcf48262e6f
yar-ermolaev <yar.ermolaev@gmail.com>
2023-12-06 11:02:38 +0300

    Refactor products.views: delete unnecessary, TemplateView instead ListView

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
+
 urlpatterns = [
-    path('', views.CategoryList.as_view(), name='index'),
-    path('categories/<slug:slug>/', views.CategoryProducts.as_view(), name='category_detail'),
-    path('categories/<slug:cat_slug>/<slug:slug>/', views.ProductView.as_view(), name='product_detail'),
+    path('', TemplateView.as_view(
+        template_name='products/show_categories.html'
+        ), name='index'),
+    path('categories/<slug:slug>/',
+         views.CategoryProducts.as_view(), name='category_detail'),
+    path('categories/<slug:cat_slug>/<slug:slug>/',
+         views.ProductView.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,34 +1,13 @@
 from pathlib import Path
 
 from django.views.generic import DetailView, ListView
-from .models import Product, Category
+from .models import Product
 
 
 class ProductView(DetailView):
     model = Product
     template_name = 'products/show_product.html'
     context_object_name = 'product'
-    # slug_field = 'slug'
-    # slug_url_kwarg = 'slug'
-
-
-class CategoryList(ListView):
-    model = Category
-    template_name = 'products/show_categories.html'
-    context_object_name = 'categories'
-
-
-class CategoryDetails(DetailView):
-    model = Category
-    template_name = 'products/category_details.html'
-    context_object_name = 'category'
-    # slug_field = 'slug'
-    # slug_url_kwarg = 'slug'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['products'] = self.object.products.all()[:10]
-        return context
 
 
 class CategoryProducts(ListView):
