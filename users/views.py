f77aa90e4d91dc4e58cfee4eaea4a06d14994956
yar-ermolaev <yar.ermolaev@gmail.com>
2023-12-06 13:28:27 +0300

    Refactor: dusers.views - elete Ðunused import

--- users/views.py
+++ users/views.py
@@ -1,8 +1,6 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
-
 from . import forms
 
 
