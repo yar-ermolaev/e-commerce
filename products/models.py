9484a4bdd11ced62b50f93b59fe654b1a1e48a5e
yar-ermolaev <yar.ermolaev@gmail.com>
2023-12-10 17:47:09 +0300

    Add method get_absolute_url to Product model and templates

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse_lazy
 
 
 class Product(models.Model):
@@ -22,6 +23,10 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse_lazy('product_detail', args=[self.category.slug,
+                                                    self.slug])
 
 
 class ProductImage(models.Model):
