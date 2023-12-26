9470b3b06d9974f7e6daed084e3fc0b431a7587a
yar-ermolaev <yar.ermolaev@gmail.com>
2023-12-26 10:11:17 +0300

    Refactor: users templates, forms

--- users/forms.py
+++ users/forms.py
@@ -4,29 +4,19 @@ from django import forms
 
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(label='Логин')
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
-
     class Meta:
         model = get_user_model()
         fields = ['username', 'password']
 
 
 class RegistrationForm(UserCreationForm):
-    username = forms.CharField(label='Логин')
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput)
-
     class Meta:
         model = get_user_model()
         fields = ['username', 'email', 'first_name',
                   'last_name', 'password1', 'password2']
-        labels = {'email': 'Email', 'first_name': 'Имя', 'last_name': 'Фамилия'}
 
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ['username', 'email', 'first_name', 'last_name']
-        labels = {'username': 'Логин', 'email': 'Email',
-                  'first_name': 'Имя', 'last_name': 'Фамилия'}
