310aa1ad8d654f3e54d3e15c5ccbbeeb8a0ab59b
yar-ermolaev <yar.ermolaev@gmail.com>
2024-01-02 16:12:41 +0300

    Add field session_key to model Cart, created - to CartItem

--- cart/models.py
+++ cart/models.py
@@ -9,9 +9,11 @@ class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,
                                 related_name='cart')
     products = models.ManyToManyField('products.Product', through='CartItem')
+    session_key = models.CharField(max_length=32, null=True, blank=True)
 
 
 class CartItem(models.Model):
     cart = models.ForeignKey('Cart', on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
