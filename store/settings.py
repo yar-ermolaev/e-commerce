0a0f7bc6c08d38210693f52252f2c3d36b26e74b
yar-ermolaev <yar.ermolaev@gmail.com>
2024-01-03 20:08:53 +0300

    Yandex smtp.EmailBackend

--- store/settings.py
+++ store/settings.py
@@ -141,5 +141,10 @@ LOGOUT_REDIRECT_URL = 'index'
 AUTH_USER_MODEL = 'users.User'
 
 # email
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST_USER = 'v0lttech@yandex.ru'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
