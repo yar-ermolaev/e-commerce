14512bd3c1bf4f9a5e70d7bf7f30702536cc93e5
yar-ermolaev <yar.ermolaev@gmail.com>
2024-01-04 12:35:54 +0300

    Add search feature, rename template products/category_details to product_list.html

--- products/urls.py
+++ products/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(
-        template_name='products/show_categories.html'
-        ), name='index'),
-    path('categories/<slug:slug>/',
-         views.CategoryProducts.as_view(), name='category_detail'),
-    path('categories/<slug:cat_slug>/<slug:slug>/',
-         views.ProductView.as_view(), name='product_detail'),
+    path('', TemplateView.as_view(template_name='products/show_categories.html'),
+         name='index'),
+    path('search/', views.SearchProducts.as_view(), name='search'),
+    path('categories/<slug:slug>/', views.CategoryProducts.as_view(),
+         name='category_detail'),
+    path('categories/<slug:cat_slug>/<slug:slug>/', views.ProductView.as_view(),
+         name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,4 @@
-from pathlib import Path
-
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.views.generic import DetailView, ListView
 from .models import Product
 
@@ -11,13 +10,22 @@ class ProductView(DetailView):
 
 
 class CategoryProducts(ListView):
-    model = Product
-    template_name = 'products/category_details.html'
+    template_name = 'products/product_list.html'
     context_object_name = 'products'
     paginate_by = 10
 
-    def get_queryset(self, **kwargs):
-        queryset = super().get_queryset(**kwargs)
-        return queryset.filter(category__slug=self.kwargs['slug'])
+    def get_queryset(self):
+        return Product.objects.filter(category__slug=self.kwargs.get('slug'))
 
 
+class SearchProducts(ListView):
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
+    paginate_by = 10
+
+    def get_queryset(self):
+        query = self.request.GET.get('q', '')
+        vector = SearchVector('name', 'description')
+        query = SearchQuery(query)
+        return Product.objects.annotate(
+            rank=SearchRank(vector, query)).filter(rank__gt=0).order_by('-rank')
