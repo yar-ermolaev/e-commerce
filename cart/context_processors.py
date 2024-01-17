4fc2b2676fbdb1a8dcddcc1ea3e5c81cb891111c
yar-ermolaev <yar.ermolaev@gmail.com>
2024-01-17 10:12:21 +0300

    Add context processor get_cart

---
+++ cart/context_processors.py
@@ -0,0 +1,12 @@
+from .models import Cart
+
+
+def get_cart(request):
+    if not request.user.is_authenticated:
+        if not request.session.session_key:
+            request.session.create()
+        cart, _ = Cart.objects.get_or_create(session_key=request.session.session_key)
+    else:
+        cart, _ = Cart.objects.get_or_create(user=request.user)
+    return {'cart': cart}
+
--- store/settings.py
+++ store/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'products.context_processors.get_categories',
+                'cart.context_processors.get_cart',
             ],
         },
     },
