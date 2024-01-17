c926d8ce5a2969ee9cf98ed2d81bb88fe6192a21
yar-ermolaev <yar.ermolaev@gmail.com>
2024-01-17 10:11:22 +0300

    Add __str__ for cart models

--- cart/models.py
+++ cart/models.py
@@ -11,9 +11,18 @@ class Cart(models.Model):
     products = models.ManyToManyField('products.Product', through='CartItem')
     session_key = models.CharField(max_length=32, null=True, blank=True)
 
+    def __str__(self):
+        if self.user:
+            return f'Корзина пользователя {self.user}'
+        else:
+            return f'Корзина анонимного пользователя, ключ сессии {self.session_key}'
+
 
 class CartItem(models.Model):
     cart = models.ForeignKey('Cart', on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     created = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+
+    def __str__(self):
+        return f'Товар {self.product} - {self.cart}'
