285499d56ab7156d1f011d639cc690702cf75c8d
yar-ermolaev <yar.ermolaev@gmail.com>
2024-01-21 10:45:36 +0300

    Refactor: create custom class AllValuesByCategory for manufacturer choices

--- products/filters.py
+++ products/filters.py
@@ -3,19 +3,22 @@ import django_filters
 from .models import Product
 
 
-class ProductFilter(django_filters.FilterSet):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        manufacturers = Product.objects.filter(
-            category__slug=self.request.resolver_match.kwargs.get('slug')).values_list(
-            'manufacturer', flat=True).distinct().order_by('manufacturer')
-        self.filters['manufacturer'].extra['choices'] = [(choice, choice) for choice in manufacturers]
+class AllValuesByCategory(django_filters.MultipleChoiceFilter):
+    @property
+    def field(self):
+        qs = self.parent.queryset
+        qs = qs.order_by(self.field_name).values_list(self.field_name, flat=True).distinct()
+        self.extra["choices"] = [(o, o) for o in qs]
+        return super().field
 
+
+class ProductFilter(django_filters.FilterSet):
     price = django_filters.RangeFilter(
         field_name='price', lookup_expr='range', label='Цена',
     )
-    manufacturer = django_filters.MultipleChoiceFilter(
-        label='Бренд', choices=[],
+
+    manufacturer = AllValuesByCategory(
+        field_name='manufacturer', label='Бренд'
     )
 
     ordering = django_filters.OrderingFilter(
